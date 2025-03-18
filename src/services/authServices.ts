04346a7eb13db2225e02594890ec6915aa31c797
Emanuel Quintino <emanuelquintino@hotmail.com>
2025-03-18 19:18:20 -0300

    add env define

--- src/services/authServices.ts
+++ src/services/authServices.ts
@@ -19,10 +19,6 @@ export const authServices = {
         throw new AppError("email or password invalid", 401);
       }
 
-      if (!process.env.SECRET_TOKEN) {
-        throw new AppError("SECRET_TOKEN is not defined", 500);
-      }
-
       const token = sign({ id: user.id }, process.env.SECRET_TOKEN, {
         expiresIn: "30s",
       });
